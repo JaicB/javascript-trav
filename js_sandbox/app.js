979aa38b623658f923a03b538c81d0150358efae
Jake Bryant <jaicb23@gmail.com>
2021-08-31 11:09:49 -0700

    0831.07

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,47 +1,47 @@
-// Built in Constructors------------------------
-// Strings
-const name1 = 'Jeff';
-const name2 = new String('Jeff');
+// Prototypes----------------------------
+// Object.prototype
+// Person.prototype
 
-// name2.foo = 'bar';
-// console.log(name2);
+function Person(firstName, lastName, dob){
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.birthday = new Date(dob);
+  // this.calculateAge = function(){
+  //   const diff = Date.now() - this.birthday.getTime();
+  //   const ageDate = new Date(diff);
+  //   return Math.abs(ageDate.getUTCFullYear() - 1970);
+  // }
+};
 
-console.log(typeof name2);
-
-if(name === 'Jeff') {
-  console.log('YES');
-} else {
-  console.log('NO');
+// Calculate age
+Person.prototype.calculateAge = function(){
+  const diff = Date.now() - this.birthday.getTime();
+  const ageDate = new Date(diff);
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
 }
 
-// Number
-const num1 = 5;
-const num2 = new Number(5);
-
-// Boolean
-const bool1 = true;
-const bool2 = new Boolean(true);
-
-// Function
-const getSum1 = function(x, y){
-  return x +y;
+// Get full name
+Person.prototype.getFullName = function(){
+  return `${this.firstName} ${this.lastName}`;
 }
 
-const getSum2 = new Function('x','y', 'return 1 + 1');
+// Gets married
+Person.prototype.getsMarried = function(newLastName){
+  this.lastName = newLastName;
+}
 
-// Object
-const john1 = {name: 'John'};
-const john2 = new Object({name: 'John'});
-console.log(john2);
+const john = new Person('John', 'Doe', '08-12-90');
+const mary = new Person('Mary', 'Johnson', 'March 20, 1978');
 
-// Arrays
-const arr1 = [1, 2, 3, 4];
-const arr2 = new Array(1, 2, 3, 4);
+console.log(mary);
 
-console.log(arr2);
+console.log(john.calculateAge());
 
-// Regular Expressions
-const re1 = /\w+/;
-const re2 = new RegExp('\\w+');
+console.log(mary.getFullName());
 
-console.log(re2);
+mary.getsMarried('Smith');
+
+console.log(mary.getFullName());
+
+console.log(mary.hasOwnProperty('firstName'));
+console.log(mary.hasOwnProperty('getFullName'));
