b6c908852427b60097a00ba7f95f0fa8bad3c276
Jake Bryant <jaicb23@gmail.com>
2021-10-20 14:57:04 -0700

    102021.02

--- api_projects/github_finder/github.js
+++ api_projects/github_finder/github.js
@@ -2,6 +2,8 @@ class Github {
   constructor() {
     this.client_id = "75e062627abcaa2ef709";
     this.client_secret = "297a51738a8d006d5a2b77ad3c71ec9fd44eaa70";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
   }
 
   async getUser(user) {
@@ -9,10 +11,16 @@ class Github {
       `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret${this.client_secret}`
     );
 
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret${this.client_secret}`
+    );
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
       profile,
+      repos,
     };
   }
 }
